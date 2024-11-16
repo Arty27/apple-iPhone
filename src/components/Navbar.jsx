ee1ba238619c773aed66c288239d49d07320545e
Arty27 <vinayn.1999@gmail.com>
2024-11-16 04:22:58 +0000

    develop hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { appleImg, bagImg, searchImg } from "../utils";
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 flex justify-between items-center">
+    <header className="w-full py-5 px-5 sm:px-10 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="apple" width={14} height={18} />
         <div className="flex flex-1 justify-center max-sm:hidden">
