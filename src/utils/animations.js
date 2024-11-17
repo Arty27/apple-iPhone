04ab49904cec1c5527c9be03b55e06b1d62698a0
Arty27 <vinayn.1999@gmail.com>
2024-11-17 02:15:33 +0000

    features section complete

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,7 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 export const animateWithGsapTimeline = (
   timeline,
   rotationRef,
@@ -27,4 +31,16 @@ export const animateWithGsapTimeline = (
     },
     "<"
   );
+};
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
 };
